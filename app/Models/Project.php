1ce885a254ed9c775300d488400087149d3dd980
Ombuweb <33325822+Nandee89@users.noreply.github.com>
2021-06-09 22:00:36 +0200

    Project can get total time of all tasks

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,10 +4,41 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Project extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name','slug','client_id'];
+    protected $fillable = ['name', 'slug', 'client_id'];
+
+    protected $touches = ['client'];
+
+    public function tasks()
+    {
+
+        return $this->hasMany(Task::class);
+    }
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+    public function timeSpentSoFar()
+    {
+        //get all projects tasks and sum up the time
+        //1. add seconds and 
+
+        $seconds = $this->totalTimeSpentSoFar();
+        $hours = floor($seconds / 3600);
+        $mins = floor($seconds / 60 % 60);
+        $secs = floor($seconds % 60);
+        $timePadded = str_pad($hours, 2,'0' ,STR_PAD_LEFT). ':' .str_pad($mins, 2, '0',STR_PAD_LEFT)  . ':' . str_pad($secs, 2,'0',STR_PAD_LEFT);
+        return $timePadded;
+    }
+
+    public function totalTimeSpentSoFar(){
+        return $this->tasks->reduce(function($carry,$d){
+            return $carry + $d->time_spent_on_secs;
+        });
+    }
 }
